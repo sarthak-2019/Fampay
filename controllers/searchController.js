8e5824c92da06beeb31c3a7307ab0e030e248945
Sarthak Shukla <sarthak2019fractal@gmail.com>
2022-08-29 20:13:07 +0530

    Integrated Api to search keywords

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,13 +4,52 @@ const handleApi = require("./handleApi");
 exports.getAllVideos = handleApi.getAll(VideoDataModel);
 
 exports.getVideos = async (req, res) => {
+  const queryObj = { ...req.query };
+  const page = queryObj.page * 1 || 1;
+  const limit = queryObj.limit * 1 || 100;
+  const skip = (page - 1) * limit;
   try {
-    result = await VideoDataModel.aggregate([
+    const result = await VideoDataModel.aggregate([
       {
         $search: {
           index: "autocomplete",
           autocomplete: {
-            query: "chill",
+            query: queryObj.word,
+            path: "title",
+            fuzzy: {
+              maxEdits: 1,
+            },
+            tokenOrder: "sequential",
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          videoId: 1,
+          channelTitle: 1,
+          publishTime: 1,
+          title: 1,
+          description: 1,
+          thumbnails: 1,
+          tag: 1,
+          score: { $meta: "searchScore" },
+        },
+      },
+      { $sort: { publishTime: -1 } },
+      {
+        $skip: skip,
+      },
+      {
+        $limit: 5,
+      },
+    ]);
+    const result1 = await VideoDataModel.aggregate([
+      {
+        $search: {
+          index: "autocomplete",
+          autocomplete: {
+            query: queryObj.word,
             path: "title",
             fuzzy: {
               maxEdits: 1,
@@ -33,11 +72,13 @@ exports.getVideos = async (req, res) => {
         },
       },
     ]);
-    console.log(result);
-    size = result.length;
-    return res.send({
-      size,
-      success: result,
+    return res.status(200).json({
+      status: "success",
+      results: result.length,
+      total_length: result1.length,
+      data: {
+        data: result,
+      },
     });
   } catch (error) {
     console.error(error);
